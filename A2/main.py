e3e48f30d7737a4889410de53dbdcf229929129d
Atheriz-46 <49459489+Atheriz-46@users.noreply.github.com>
2021-10-19 00:05:47 +0530

    Update main.py
    updated IOU

--- A2/main.py
+++ A2/main.py
@@ -54,7 +54,18 @@ def NCC(t,f):
     return np.sum(f*t)/np.std(f)
 
 def IOU(box1,box2):
-    pass
+    boxA,boxB = box1,box2
+    boxA[2:]+=boxA[:2]
+    boxB[2:]+=boxB[:2]
+    xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+	return iou
 
 def block_based(dir,p_0,delp,n_p,outfile,metric = NSSE):
     sample = [np.linspace(-dp,dp,n) for dp,n in zip(delp,n_p)]
