3e52b6d8c32055fe19c3bc95f343ee32e2465e07
Atheriz-46 <49459489+Atheriz-46@users.noreply.github.com>
2021-10-28 18:16:11 +0530

    Started Part b

--- A3/HOG.py
+++ A3/HOG.py
@@ -1,10 +1,11 @@
-import cv2 as cv
-import numpy as np
-import os
-from imutils.object_detection import non_max_suppression
-from imutils import paths
-import imutils
+
 def HOG_Predefined(inp_path = 'A3\data\PNGImages',padding=(8, 8),winStride=(4, 4),scale=1.05,probs=None, overlapThresh=0.65,wid=400)
+    import cv2 as cv
+    import numpy as np
+    import os
+    from imutils.object_detection import non_max_suppression
+    from imutils import paths
+    import imutils
     hog = cv.HOGDescriptor()
     hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
     # inp_path = 'A3\data\PNGImages'
@@ -26,6 +27,26 @@ def HOG_Predefined(inp_path = 'A3\data\PNGImages',padding=(8, 8),winStride=(4, 4
             for (xA, yA, xB, yB) in pick:
                 cv.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
             cv.imshow("Dectector", image)
+
+            if cv.waitKey(30)=='q':
+                break
+
+
+
+
+def HOG_train(inp_path = 'A3\data\PNGImages'):
+    import cv2 as cv
+    import numpy as np
+    import os
+    from imutils.object_detection import non_max_suppression
+    from imutils import paths
+    import imutils
+    files = sorted(os.listdir(inp_path))
+    for imagePath in files:
+        if imagePath[-4:]=='.png':
+            image = cv.imread(os.path.join(inp_path,imagePath))
+    
+        cv.imshow("Dectector", image)
 
             if cv.waitKey(30)=='q':
                 break
